b85fe76538dd533583a9d0d8f9f400fb4fe8403f
Yan JiXian <45987126+jixianyan@users.noreply.github.com>
2021-01-06 16:04:25 +0800

    Update ex-1.5.scm

--- chapter1/ex-1.5.scm
+++ chapter1/ex-1.5.scm
@@ -1,5 +1,12 @@
-;Applicative-order: evaluate the arguments and then apply. This model of evaluation starts by not finding the values of the operator objects until their values are actually needed. The formal arguments are first replaced by the operator expressions until an expression containing only the basic operators is obtained, and then the evaluation is performed.
-;Normal-order: fully expand and then reduce. The interpreter will first evaluate each element of the combinatorial formula, i.e., replace each formal parameter in the procedure body with a real parameter, and then evaluate this procedure body.
+;Applicative-order: evaluate the arguments and then apply.
+;This model of evaluation starts by not finding the values of the operator objects until their values are actually needed.
+;The formal arguments are first replaced by the operator expressions until an expression containing only the basic operators is obtained,
+;and then the evaluation is performed.
+
+;Normal-order: fully expand and then reduce.
+;The interpreter will first evaluate each element of the combinatorial formula,
+;i.e.,replace each formal parameter in the procedure body with a real parameter,
+;and then evaluate this procedure body.
 
 ;Using applicative-order evaluation, the evaluation of (test 0 (p)) never terminates, because (p) is infinitely expanded itself:
 (test 0 (p))
